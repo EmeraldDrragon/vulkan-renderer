8099bcdda63fbc07f29f652eb2181cce35a18b9a
Salamat Munipov <munipov@student.tugraz.at>
2026-03-05 11:12:51 +0100

    Refactoring, image alloc

--- source/ImageAlloc.h
+++ source/ImageAlloc.h
@@ -12,6 +12,20 @@ public:
 
     ImageAlloc() = default;
 
-    static ImageAlloc create(VmaAllocator allocator, VkDevice device, VkImageCreateInfo image_create_info,)
+    static ImageAlloc create(VmaAllocator allocator, 
+        VkDevice device, 
+        VkImageCreateInfo image_create_info, 
+        VmaAllocationCreateFlags vma_flags, 
+        VkImageAspectFlags aspect_mask)
+    {
+        ImageAlloc img;
+        img.allocator = allocator;
+        VmaAllocationCreateInfo alloc_create_info = {
+            .flags = VMA_ALLOCATION_CREATE_DEDICATED_MEMORY_BIT,
+            .usage = VMA_MEMORY_USAGE_AUTO
+        };
+        vmaCreateImage(allocator, &image_create_info, &alloc_create_info, &img.handle, &img.allocation, nullptr);
+
+    }
 
 };  
